306eaec49cb52012cd3d80a9f28092674a77b3f4
emirbaki <74327252+emirbaki@users.noreply.github.com>
2023-02-27 00:26:59 +0300

    general update about shopcontroller with crud actions

--- Homate/Controllers/ShopController.cs
+++ Homate/Controllers/ShopController.cs
@@ -1,6 +1,10 @@
 ﻿using Homate.Data;
+using Homate.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace Homate.Controllers
 {
@@ -31,13 +35,77 @@ namespace Homate.Controllers
         public IActionResult GetShopByID(int id)
         {
             var a = Shops.shops.Find(x => x.Id == id);
-            var element = Shops.shops.Where(x => x.Id == id).FirstOrDefault();
 
             if (a is null)
             {
                 return NotFound("Bulamadık la");
             }
+            _ilogger.LogInformation("E bulduk aga");
             return StatusCode(200, a);
+        }
+
+        [HttpPost]
+        public IActionResult SubmitShop([FromBody] Shop shop)
+        {
+            if (Shops.shops.Contains(shop) || Shops.shops.Any(x => x.Id == shop.Id))
+            {
+                return BadRequest("Bu Shop halihazırda var");
+            }
+            else
+            {
+                Shops.shops.Add(shop);
+                return StatusCode(200, shop);
+
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateShop(int id, [FromBody] Shop shop)
+        {
+            var element = Shops.shops.Find(x => x.Id == id);
+            if (id != shop.Id)
+            {
+                return BadRequest("Headerdaki ID ile Body'deki ID uyuşmuyor");
+            }
+            else if(element is null)
+            {
+                return NotFound("Bulamadık la");
+            }
+
+            element.Id = shop.Id;
+            element.Name = shop.Name;
+            element.Description = shop.Description;
+
+            return StatusCode(200, element);
+        }
+
+        [HttpPatch("{id:int}")]
+        public IActionResult PartiallyUpdateShop([FromRoute] int id, [FromBody] JsonPatchDocument<Shop> document)
+        {
+            var element = Shops.shops.Find(x => x.Id == id);
+
+            if(element is null)
+            {
+                return NotFound("Bulamadık la");
+            }
+            document.ApplyTo(element);
+            return StatusCode(200);
+        }
+
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteShopByID(int id)
+        {
+            var element = Shops.shops.Find(x => x.Id == id);
+            if (element is null)
+            {
+                return NotFound("Bulamadık la");
+            }
+            else
+            {
+                Shops.shops.Remove(element);
+            }
+            return StatusCode(200, element);
         }
     }
 }
